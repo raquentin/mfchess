46d6bfc0c5abd7faa524f9463b79a839dfc9f0a2
Giovanni Hsu <giovannihsu.123@gmail.com>
2023-03-14 17:42:42 -0400

    env for frontend

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //* Create an Axios Instance so we'll only need to change the url once.
 const instance = axios.create({
-    baseURL: 'https://86.48.25.224/:3002'
+    baseURL: process.env.PORT
 })
 
 export default instance;
