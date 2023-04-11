1a445ed1f44c2f073a4024fd91024744464f187a
r4c3 <rqwilliams9@gmail.com>
2023-04-11 19:03:58 -0400

    port to 443

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //* Create an Axios Instance so we'll only need to change the url once.
 const instance = axios.create({
-    baseURL: 'https://api.mfchess.com:3002'
+    baseURL: 'https://api.mfchess.com:443'
 })
 
 export default instance;
