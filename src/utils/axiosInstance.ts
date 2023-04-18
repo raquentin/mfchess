b4cbdb1fe388b2449cb40dd005aab9982b5cdd69
r4c3 <rqwilliams9@gmail.com>
2023-04-18 18:10:24 -0400

    switch to backend

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 //* Create an Axios Instance so we'll only need to change the url once.
 const instance = axios.create({
     // baseURL: 'https://api.mfchess.com:443'
-    baseURL: 'http://localhost:3002'
+    baseURL: 'https://mfchess-backend.onrender.com:3002'
 })
 
 export default instance;
